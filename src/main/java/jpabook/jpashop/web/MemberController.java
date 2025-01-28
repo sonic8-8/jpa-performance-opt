aade2f14c2313b4b16a10519e60116434cb3a4ac
이승환 <seaung13@gmail.com>
2025-01-28 18:39:59 +0900

    feat: 웹 계층 개발 - MemberController에 회원 목록 조회 핸들러 메서드 추가, 타임리프 템플릿 뷰 추가 (memberList)

--- src/main/java/jpabook/jpashop/web/MemberController.java
+++ src/main/java/jpabook/jpashop/web/MemberController.java
@@ -12,6 +12,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -40,5 +42,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
